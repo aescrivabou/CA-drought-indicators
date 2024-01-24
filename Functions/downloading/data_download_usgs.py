db741229cb89ab04c18c37a8ad4489c7b3ed8758
armen.konia <armen.konia@gmail.com>
2024-01-24 06:53:16 -0800

    snow usgs updated and new download_data file added

--- Functions/downloading/data_download_usgs.py
+++ Functions/downloading/data_download_usgs.py
@@ -11,26 +11,30 @@ import pandas as pd
 import dataretrieval.nwis as nwis
 import os
 
-## import streamgage site list
+def download_streamflow_data(
+             startdate = '1-1-1991',
+             enddate = '12-1-2023',
+             ): 
 
-stations = pd.read_csv("../../Data/Input_Data/usgs/sg_usgs_hr.csv")
-stations.update(stations[['site']].astype(str))
-sites = list(stations['site'])
-
-
-streamflow_all = pd.DataFrame()
-
-for n in np.arange(0, len(stations), 1):
-    print(n)
-    try:
-        streamflow_data = nwis.get_record(sites=sites[n:n+1], service='dv',  start='1991-01-01', end='2023-04-01', parameterCd = "00060")
-    except:
-        streamflow_data = pd.DataFrame()
-        pass
-    streamflow_all = pd.concat([streamflow_all, streamflow_data])
+    ## import streamgage site list
     
-streamflow_all = streamflow_all.reset_index()
-streamflow_all = streamflow_all.merge(stations, left_on='site_no', right_on='site')
-
-os.makedirs('../../Data/Downloaded/usgs/', exist_ok=True)
-streamflow_all.to_csv(os.path.join('../../Data/Downloaded/usgs/streamflow_daily_data.csv'))
+    stations = pd.read_csv("../../Data/Input_Data/usgs/sg_usgs_hr.csv")
+    stations.update(stations[['site']].astype(str))
+    sites = list(stations['site'])
+    
+    
+    streamflow_all = pd.DataFrame()
+    
+    for n in np.arange(0, len(stations), 1):
+        print(n)
+        try:
+            streamflow_data = nwis.get_record(sites=sites[n:n+1], service='dv',  start='1991-01-01', end='2023-04-01', parameterCd = "00060")
+        except:
+            streamflow_data = pd.DataFrame()
+            pass
+        streamflow_all = pd.concat([streamflow_all, streamflow_data])
+        
+    streamflow_all = streamflow_all.reset_index()
+    streamflow_all = streamflow_all.merge(stations, left_on='site_no', right_on='site')
+    
+    return streamflow_all
--- Functions/downloading/download_cdec_snow.py
+++ Functions/downloading/download_cdec_snow.py
@@ -10,60 +10,71 @@ import pandas as pd
 from tqdm import tqdm
 import os
 
-# import snotel list     
-snotels = pd.read_csv('../../Data/Input_Data/cdec/snotels3.csv')
+## define downloading function
+def download_snow_data(
+             startdate = '1-1-1991',
+             enddate = '6-1-2023',
+             ): 
 
-
-startdate = '1-1-1991'
-enddate = '6-1-2023'
-sensor_num = 82
-dur_code = 'D'
-
-snow = pd.DataFrame()
-for stn_name in tqdm(snotels.station):
-    url = f'https://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?Stations={stn_name}&SensorNums={sensor_num}&dur_code={dur_code}&Start={startdate}&End={enddate}'
-    datasnow01 = pd.read_csv(url, on_bad_lines='skip')
-    snow = pd.concat([snow, datasnow01])
-
-# snow = pd.read_csv("C:/Users/armen/Desktop/snow.csv")
-snow['DATE TIME'] = pd.to_datetime(snow['DATE TIME'])
-
-# calculate April 1st 'normal' for each sensor
-snow['month'] = snow['DATE TIME'].dt.month
-snow['year'] = snow['DATE TIME'].dt.year
-snow['day'] = snow['DATE TIME'].dt.day
-
-snow['VALUE'] = pd.to_numeric(snow['VALUE'], errors='coerce')
-snow_normal = snow.loc[(snow['month'] == 4) & (snow['day'] == 1)]
-snow_normal = snow_normal.dropna(subset=['VALUE'])
-snow_normal_ave = snow_normal.groupby(['STATION_ID']).mean(numeric_only=True).reset_index()
-snow_normal_ave = snow_normal_ave[['STATION_ID', 'VALUE']]
-snow_normal_ave = snow_normal_ave.rename(columns={'VALUE': 'normal'})
-merged = pd.merge(snow, snow_normal_ave, on='STATION_ID')
-
-# calculate a percentile for each daily SWC value
-merged['percent_normal'] = merged['VALUE']/merged['normal']
-
-# calculate monthly ave of the 'percentiles'
-all = pd.merge(merged, snotels, left_on='STATION_ID', right_on='station')
-basin = all.groupby(['Basin','year','month']).mean(numeric_only=True).reset_index()
-basin = basin[['Basin', 'year', 'month', 'percent_normal']]
-
-regions = snotels.groupby(['Basin', 'HR']).mean(numeric_only=True).reset_index()
-basin_HR = pd.merge(basin, regions, on='Basin')
-
-# multiply average percentile by Margulis' estimate for April 1st regional SWE (converted to AF)
-basin_HR['SWC'] = basin_HR['percent_normal']*basin_HR['aprilmean']*810714
-basin_HR = basin_HR[['Basin', 'HR', 'year', 'month', 'percent_normal', 'aprilmean', 'SWC']]
-
-# summarize by region
-regional = basin_HR.groupby(['HR','year','month']).sum().reset_index()
-regional['HR_NAME']=regional.HR
-regional = regional[['HR_NAME', 'year', 'month', 'SWC']]
-
-snow = pd.merge(snow, snotels, left_on='STATION_ID', right_on='station')
-
-
-os.makedirs('../../Data/Downloaded/cdec/snow/', exist_ok=True)
-snow.to_csv(os.path.join('../../Data/Downloaded/cdec/snow/snow_stations.csv'))
-regional.to_csv(os.path.join('../../Data/Downloaded/cdec/snow/SnowRegional.csv'))
+        
+    # import snotel list     
+    snotels = pd.read_csv('../../Data/Input_Data/cdec/snotels3.csv')
+    
+    
+    startdate = '1-1-1991'
+    enddate = '6-1-2023'
+    sensor_num = 82
+    dur_code = 'D'
+    
+    snow = pd.DataFrame()
+    for stn_name in tqdm(snotels.station):
+        url = f'https://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?Stations={stn_name}&SensorNums={sensor_num}&dur_code={dur_code}&Start={startdate}&End={enddate}'
+        datasnow01 = pd.read_csv(url, on_bad_lines='skip')
+        snow = pd.concat([snow, datasnow01])
+    
+    # snow = pd.read_csv("C:/Users/armen/Desktop/snow.csv")
+    snow['DATE TIME'] = pd.to_datetime(snow['DATE TIME'])
+    
+    # calculate April 1st 'normal' for each sensor
+    snow['month'] = snow['DATE TIME'].dt.month
+    snow['year'] = snow['DATE TIME'].dt.year
+    snow['day'] = snow['DATE TIME'].dt.day
+    
+    snow['VALUE'] = pd.to_numeric(snow['VALUE'], errors='coerce')
+    snow_normal = snow.loc[(snow['month'] == 4) & (snow['day'] == 1)]
+    snow_normal = snow_normal.dropna(subset=['VALUE'])
+    snow_normal_ave = snow_normal.groupby(['STATION_ID']).mean(numeric_only=True).reset_index()
+    snow_normal_ave = snow_normal_ave[['STATION_ID', 'VALUE']]
+    snow_normal_ave = snow_normal_ave.rename(columns={'VALUE': 'normal'})
+    merged = pd.merge(snow, snow_normal_ave, on='STATION_ID')
+    
+    # calculate a percentile for each daily SWC value
+    merged['percent_normal'] = merged['VALUE']/merged['normal']
+    
+    # calculate monthly ave of the 'percentiles'
+    all = pd.merge(merged, snotels, left_on='STATION_ID', right_on='station')
+    basin = all.groupby(['Basin','year','month']).mean(numeric_only=True).reset_index()
+    basin = basin[['Basin', 'year', 'month', 'percent_normal']]
+    
+    regions = snotels.groupby(['Basin', 'HR']).mean(numeric_only=True).reset_index()
+    basin_HR = pd.merge(basin, regions, on='Basin')
+    
+    # multiply average percentile by Margulis' estimate for April 1st regional SWE (converted to AF)
+    basin_HR['SWC'] = basin_HR['percent_normal']*basin_HR['aprilmean']*810714
+    basin_HR = basin_HR[['Basin', 'HR', 'year', 'month', 'percent_normal', 'aprilmean', 'SWC']]
+    
+    # summarize by region
+    regional = basin_HR.groupby(['HR','year','month']).sum().reset_index()
+    regional['HR_NAME']=regional.HR
+    regional = regional[['HR_NAME', 'year', 'month', 'SWC']]
+    
+    snow = pd.merge(snow, snotels, left_on='STATION_ID', right_on='station')
+    
+    os.makedirs('../../Data/Downloaded/cdec/snow/', exist_ok=True)
+    snow.to_csv(os.path.join('../../Data/Downloaded/cdec/snow/snow_stations.csv'))
+    regional.to_csv(os.path.join('../../Data/Downloaded/cdec/snow/SnowRegional.csv'))
+    
+download_snow_data(
+             startdate = '1-1-1991',
+             enddate = '6-1-2023',
+             )
